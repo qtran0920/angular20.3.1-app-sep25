0142b106aabbddf1f6722f2170eca4cf90f8ff67
Jimmy Tran <qtran11@gmu.edu>
2025-09-15 14:08:59 -0400

    feat(user): output dynamic content with string interpolation

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
 @Component({
   selector: 'app-user',
   imports: [],
@@ -7,5 +11,5 @@ import { Component } from '@angular/core';
   styleUrl: './user.css'
 })
 export class User {
-
+  selectedUser = DUMMY_USERS[randomIndex];
 }
